11370301c5ebe9f95f284f4ec56b1a76ea761ab0
Keks <keks@htmlacademy.ru>
2021-12-25 17:09:23 +0300

    :heavy_check_mark: Сборка #10

---
+++ 10/js/script.js
@@ -0,0 +1,38 @@
+var navSite = document.querySelector('.site-nav');
+var navUser = document.querySelector('.user-nav');
+var navToggle = document.querySelector('.page-header__toggle');
+
+
+navSite.classList.remove('page-header__nav--nojs');
+navUser.classList.remove('page-header__nav--nojs');
+navToggle.classList.remove('page-header__toggle--nojs');
+
+navSite.classList.add('page-header__nav--closed');
+navUser.classList.add('page-header__nav--closed');
+navToggle.classList.add('page-header__toggle--closed');
+
+navToggle.addEventListener('click', function () {
+  if (navSite.classList.contains('page-header__nav--closed')) {
+    navSite.classList.remove('page-header__nav--closed');
+    navSite.classList.add('page-header__nav--open');
+  } else {
+    navSite.classList.add('page-header__nav--closed');
+    navSite.classList.remove('page-header__nav--open');
+
+  }
+  if (navUser.classList.contains('page-header__nav--closed')) {
+    navUser.classList.remove('page-header__nav--closed');
+    navUser.classList.add('page-header__nav--open');
+  } else {
+    navUser.classList.add('page-header__nav--closed');
+    navUser.classList.remove('page-header__nav--open');
+  }
+
+  if (navToggle.classList.contains('page-header__toggle--closed')) {
+    navToggle.classList.remove('page-header__toggle--closed');
+    navToggle.classList.add('page-header__toggle--open');
+  } else {
+    navToggle.classList.add('page-header__toggle--closed');
+    navToggle.classList.remove('page-header__toggle--open');
+  }
+});
